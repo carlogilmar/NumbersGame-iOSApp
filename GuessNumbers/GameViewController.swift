96566dedbf786037a6fe8bb855208d3b9554ea10
Carlo Gilmar <carlogilmar12@gmail.com>
2018-03-20 02:00:05 -0600

    Adding labels and buttons and actions in GameViewController

---
+++ GuessNumbers/GameViewController.swift
@@ -0,0 +1,44 @@
+//
+//  GameViewController.swift
+//  GuessNumbers
+//
+//  Created by Carlo Gilmar on 3/20/18.
+//  Copyright © 2018 Carlo Gilmar. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var coinLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var rightButton: UIButton!
+    
+    @IBAction func startButtonDidTap(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func leftButtonDidTap(_ sender: UIButton) {
+    
+    }
+    
+    @IBAction func rightButtonDidTap(_ sender: UIButton) {
+    
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
